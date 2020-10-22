ca52f52989f804003813456c22b2a28fc7a96b3b
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-22 10:05:27 +0200

    bodyParser and cors middlewares

--- src/app.js
+++ src/app.js
@@ -3,4 +3,12 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 
-console.log('Hello World!');
+import { APP_PORT } from './config/app.js';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(APP_PORT, () => console.log(`Server is listening on port ${APP_PORT}...`));

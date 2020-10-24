90756798c85fc135282fb7feb16bddbb07b31d0e
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-24 18:49:15 +0200

    Products router - GET handlers

--- src/api/products.js
+++ src/api/products.js
@@ -3,5 +3,36 @@ import express from 'express';
 export const productsRouter = express.Router();
 
 productsRouter.get('/', (req, res) => {
-  res.send('Products router');
+  res.json({
+    availableMethods: ['GET /all', 'GET /:id', 'POST', 'PUT /:id', 'DELETE /:id']
+  });
+});
+
+productsRouter.get('/all', (req, res) => {
+  console.log('GET Products - All available products');
+  // temporary mock
+  res.json([
+    {
+      product: 1
+    },
+    {
+      product: 2
+    }
+  ]);
+});
+
+productsRouter.get('/:id', (req, res) => {
+  console.log(`GET Product id:${req.params.id}`);
+  // temporary mock
+  res.json({
+    _id: '123',
+    name: 'Mocha',
+    brand: 'Bialetti',
+    lastOrderDate: new Date(),
+    unitPrice: 2,
+    supplierName: 'EuroKawexpol',
+    available: 10,
+    expirationDate: new Date(),
+    categories: ['coffee']
+  });
 });

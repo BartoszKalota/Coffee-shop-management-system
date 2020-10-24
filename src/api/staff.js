377f0a0ef19998c18c8bc28b9675cfbbcf33ce19
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-24 19:08:34 +0200

    Staff router - GET handlers

--- src/api/staff.js
+++ src/api/staff.js
@@ -3,5 +3,21 @@ import express from 'express';
 export const staffRouter = express.Router();
 
 staffRouter.get('/', (req, res) => {
-  res.send('Staff router');
+  res.json({
+    availableMethods: ['GET /:id', 'POST', 'PUT /:id', 'DELETE /:id']
+  });
+});
+
+staffRouter.get('/:id', (req, res) => {
+  console.log(`GET Staff id:${req.params.id}`);
+  // temporary mock
+  res.json({
+    _id: '1',
+    firstName: 'Jan',
+    lastName: 'Kowalski',
+    startedAt: new Date(),
+    rating: 4.5,
+    position: ['waiter'],
+    monthlySalary: 4000
+  });
 });

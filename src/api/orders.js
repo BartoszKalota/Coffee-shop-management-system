d3293868d84b9a4a6e1957e04d6038c2d4fe093d
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-24 18:06:18 +0200

    Orders router - GET handlers

--- src/api/orders.js
+++ src/api/orders.js
@@ -3,5 +3,28 @@ import express from 'express';
 export const ordersRouter = express.Router();
 
 ordersRouter.get('/', (req, res) => {
-  res.send('Order router');
+  res.json({
+    availableMethods: ['GET /:id', 'POST', 'PUT /:id', 'DELETE /:id']
+  });
+});
+
+ordersRouter.get('/:id', (req, res) => {
+  console.log(`GET Order id:${req.params.id}`);
+  // temporary mock
+  res.json({
+    _id: '1',
+    date: new Date(),
+    location: '2',
+    paidIn: 'cash',
+    staffId: '2',
+    products: [
+      {
+        productId: '3',
+        name: 'Mocha',
+        amount: 2,
+        unitPrice: 2,
+      },
+    ],
+    total: 4
+  });
 });

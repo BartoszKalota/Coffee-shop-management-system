70d7d8dcd49f4bab9d6bdf9697369cae723c76a4
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-25 09:11:08 +0100

    Products services - Errors constants distribution

--- src/services/products.js
+++ src/services/products.js
@@ -1,21 +1,30 @@
+import { CONFLICT, NOT_FOUND, MISSING_DATA } from '../constants/error.js';
+
 export default class Products {
   async getAllProducts() {
-    
+
   }
   
   async getProduct(productId) {
+    if (!productId) throw new Error(MISSING_DATA);
 
   }
 
   async addProduct(productData) {
+    if (!productData) throw new Error(MISSING_DATA);
+    if (productData._id === this.mockProduct._id) throw new Error(CONFLICT);
 
   }
 
   async updateProduct(productId, productData) {
+    if (!productId || !productData) throw new Error(MISSING_DATA);
+    if (productId !== this.mockProduct._id) throw new Error(NOT_FOUND);
 
   }
 
   async deleteProduct(productId) {
+    if (!productId) throw new Error(MISSING_DATA);
+    if (productId !== this.mockProduct._id) throw new Error(NOT_FOUND);
 
   }
 }

626f8add066e3e06bae10b06001ddca4191b9930
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-25 09:00:28 +0100

    Orders services - Errors constants distribution

--- src/services/orders.js
+++ src/services/orders.js
@@ -1,17 +1,26 @@
+import { CONFLICT, NOT_FOUND, MISSING_DATA } from '../constants/error.js';
+
 export default class Orders {
-  async getOrder(orderId, dateFrom, dateTo) {
+  async getOrder(orderId) {
+    if (!orderId) throw new Error(MISSING_DATA);
 
   }
 
   async addOrder(orderData) {
+    if (!orderData) throw new Error(MISSING_DATA);
+    if (orderData._id === this.mockOrder._id) throw new Error(CONFLICT);
 
   }
 
   async updateOrder(orderId, orderData) {
+    if (!orderId || !orderData) throw new Error(MISSING_DATA);
+    if (orderId !== this.mockOrder._id) throw new Error(NOT_FOUND);
 
   }
 
   async deleteOrder(orderId) {
+    if (!orderId) throw new Error(MISSING_DATA);
+    if (orderId !== this.mockOrder._id) throw new Error(NOT_FOUND);
 
   }
 }

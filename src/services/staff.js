2690c9ec91956e0b329480be79fd5bcf9e3dd85f
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-25 09:47:34 +0100

    Staff services - Errors constants distribution

--- src/services/staff.js
+++ src/services/staff.js
@@ -1,17 +1,26 @@
+import { CONFLICT, NOT_FOUND, MISSING_DATA } from '../constants/error.js';
+
 export default class Staff {
   async getEmployee(employeeId) {
+    if (!employeeId) throw new Error(MISSING_DATA);
 
   }
 
   async addEmployee(employeeData) {
+    if (!employeeData) throw new Error(MISSING_DATA);
+    if (employeeData._id === this.mockEmployee._id) throw new Error(CONFLICT);
 
   }
 
   async updateEmployee(employeeId, employeeData) {
+    if (!employeeId || !employeeData) throw new Error(MISSING_DATA);
+    if (employeeId !== this.mockEmployee._id) throw new Error(NOT_FOUND);
 
   }
 
   async deleteEmployee(employeeId) {
+    if (!employeeId) throw new Error(MISSING_DATA);
+    if (employeeId !== this.mockEmployee._id) throw new Error(NOT_FOUND);
 
   }
 }

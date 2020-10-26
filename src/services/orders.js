0dadb2b99fd3163b4dd41944d28a2c9e4faf789d
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-26 15:38:48 +0100

    Orders Joi schemas

--- src/services/orders.js
+++ src/services/orders.js
@@ -1,3 +1,5 @@
+import Joi from '@hapi/joi';
+
 import { CONFLICT, NOT_FOUND, MISSING_DATA } from '../constants/error.js';
 
 export default class Orders {
@@ -18,6 +20,29 @@ export default class Orders {
     ],
     total: 4
   };
+
+  orderedProductSchema = Joi.object().keys({
+    prodId = idSchema.required(),
+    name: Joi.string().required(),
+    amount: Joi.number().greater(0).required(),
+    unitPrice: Joi.number().greater(0).required()
+  });
+
+  orderUpdateSchema = Joi.object().keys({
+    _id: idSchema.required(),
+    date: Joi.date(),
+    location: Joi.string(),
+    paidIn: Joi.string().valid('cash', 'card'),
+    staffId: Joi.string().length(24),
+    products: Joi.array().items(this.orderedProductSchema),
+    total: Joi.number().greater(0)
+  });
+
+  orderSchema = this.orderUpdateSchema.options({ presence: 'required' });
+
+  addOrderSchema = this.orderSchema.keys({
+    _id: Joi.any().strip().optional()
+  });
 
   async getOrder(orderId) {
     if (!orderId) throw new Error(MISSING_DATA);

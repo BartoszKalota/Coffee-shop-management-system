7e3d8747d29b00b6eebd3f8d742e50822be9bf6f
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-28 21:14:52 +0100

    Create error for orders' peers

--- src/utils/errorResponse.js
+++ src/utils/errorResponse.js
@@ -1,4 +1,9 @@
-import { CONFLICT, NOT_FOUND, MISSING_DATA } from '../constants/error.js';
+import {
+  CONFLICT,
+  NOT_FOUND,
+  MISSING_DATA,
+  PEER_ERROR
+} from '../constants/error.js';
 
 export default (err, res) => {
   switch (err.message) {
@@ -8,6 +13,8 @@ export default (err, res) => {
       return res.status(404).send('Item not found');
     case MISSING_DATA:
       return res.status(400).send('Missing input data');
+    case PEER_ERROR:
+      return res.status(409).send('Peer resource does not exist');
     default:
       return res.status(500).send(`Server error: ${err.message}`);
   }

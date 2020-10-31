bd7f2b21a615e20ce4eef878afdb748383c665c7
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-31 14:54:59 +0100

    Staff remove Joi

--- src/services/staff.js
+++ src/services/staff.js
@@ -1,5 +1,3 @@
-import mongoose from 'mongoose';
-
 import { VALIDATION_ERROR } from '../constants/error.js';
 import {
   getEmployee as dbGetEmployee,
@@ -9,28 +7,6 @@ import {
 } from '../models/staff.js';
 
 export default class Staff {
-  // employeeUpdateSchema = Joi.object().keys({
-  //   _id: Joi.string().length(24).required(),
-  //   firstName: Joi.string(),
-  //   lastName: Joi.string(),
-  //   startedAt: Joi.date(),
-  //   rating: Joi.number().min(0).max(10),
-  //   position: Joi.array().items(
-  //     Joi.string().valid('waiter'),
-  //     Joi.string().valid('waitress'),
-  //     Joi.string().valid('barista'),
-  //     Joi.string().valid('cleaning'),
-  //     Joi.string().valid('temp')
-  //   ),
-  //   monthlySalary: Joi.number().min(2000)
-  // });
-
-  // employeeSchema = this.employeeUpdateSchema.options({ presence: 'required' });
-
-  // addEmployeeSchema = this.employeeSchema.keys({
-  //   _id: Joi.any().strip().optional()
-  // });
-
   async getEmployee(employeeId) {
     // db connection
     return await dbGetEmployee(employeeId);
